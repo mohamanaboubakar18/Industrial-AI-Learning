9cb01cc0724bee490713383808997d83758cf661
mohamanaboubakar18 <mohamanaboubakar18@gmail.com>
2026-06-25 10:00:08 +0200

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,8 @@
+from statistics import mean
+
+Temp_measures= [15.23,25.02,30.2,35,50]
+#Num_measures=len(Temp_measures)
+Avg_measures= mean(Temp_measures)
+
+print('La temperature moyenne est de ',Avg_measures,'°C')
+
